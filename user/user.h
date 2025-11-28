f0497ea624045ea379545d21d08f7818dba941e3
Xorzj <2351566349@qq.com>
2025-11-28 23:59:58 +0800

    LAB4

--- user/user.h
+++ user/user.h
@@ -23,7 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+void sigalarm(uint64,void(*)(void));
+void sigreturn(void);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
